06805d112101451455e5106d5bea40b29c7a44af
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2016-09-16 19:19:50 +0300

    Move tunable parameters into global variables and comment on what they do. Add explicit mention of classes that are too small to get useful results

--- recommend.py
+++ recommend.py
@@ -6,8 +6,30 @@ from collections import Counter
 import numpy as np
 import sklearn.cluster
 
+# TUNABLE PARAMETER
+# Minimum cluster size for which a tag list will be generated
+# Clusters of this size or less will be ignored when generating tag queries
+# If you're getting too little queries or no queries at all,
+# consider lowering this parameter.
+cluster_size_theshold = 3
 
-# Generate set of all known tags
+# TUNABLE PARAMETER
+# Part of the cluster that does not have to be covered by tags
+# on every iteration of generating a strict query.
+# Tunable in the range from 0 to 0.9999999999
+# If you are getting too much results (especially irrelevant ones)
+# consider setting this parameter a bit higher;
+# If you are getting only the images you have passed and little else
+# (too few results), lower this parameter.
+cluster_coverage_allowance = 0.17
+
+# Useless tags that skew clustering; it's easy to filter for these manually
+# Sort of tunable, usually sufficient to set once for an imageboard
+tag_blacklist = ["safe", "suggestive"]
+
+
+# Generates set of all known tags from lists of tags for each image
+# Required for clustering
 def list_all_tags(lists_of_tags):
     all_known_tags = set()
     for list_of_tags in lists_of_tags:
@@ -16,8 +38,9 @@ def list_all_tags(lists_of_tags):
     return list(all_known_tags) # so it would be ordered
 
 
-# Not scalable, but making hash tables for ~100 tags is an overkill
+# For clustering
 def tags_to_boolean_features(object_tags, all_known_tags):
+    # Not scalable, but making hash tables for ~100 tags is an overkill
     features = np.array( [0] * len(all_known_tags) )
     for tag in object_tags:
         try:
@@ -27,12 +50,12 @@ def tags_to_boolean_features(object_tags, all_known_tags):
     return features
 
 
-# Converts an index of an object feature into the tag string
+# Converts an index of a feature back into the tag string
 def feature_index_to_tag(index, all_known_tags):
     return all_known_tags[index]
 
 
-# Returns a collections.Counter object with feature indices
+# Returns a collections.Counter object with count for each tag
 def count_common_tags(input_objects):
     counter = Counter()
     for obj in input_objects:
@@ -52,7 +75,7 @@ for filename in tag_files:
         for raw_tag in comma_delimited_tags.split(','):
             tag = raw_tag.strip()
             # Filter out artist tags because it's trivial to browse the artist manually
-            if not (tag.startswith("artist:") or tag == "safe" or tag == "suggestive"):
+            if not (tag.startswith("artist:") or tag in tag_blacklist):
                 tags.append(tag)
         input_objects.append(tags)
 
@@ -96,7 +119,9 @@ def assemble_query_for_objects(input_objects, all_known_tags):
         iter_result_tags = []
         non_covered_objects = deepcopy(input_objects)
         # This loop is responsible for generating lists lists of tags with OR between them
-        while len(non_covered_objects) > total_objects/6: # TUNABLE PARAMETER
+        assert(cluster_coverage_allowance >= 0)
+        assert(cluster_coverage_allowance < 1)
+        while len(non_covered_objects) > total_objects * cluster_coverage_allowance: # TUNABLE PARAMETER, see top of the file
             #print(len(non_covered_objects))
             # Find most common tags in objects that are not yet covered
             iter_tag_frequencies = count_common_tags(non_covered_objects)
@@ -113,7 +138,7 @@ def assemble_query_for_objects(input_objects, all_known_tags):
             #print(iter_result_tags)
             objects_covered_by_tag = [obj for obj in non_covered_objects if (most_common_unprocessed_tag in obj)]
             if len(objects_covered_by_tag) == 0:
-            	# cannot find any more tags to cover more of the objects 
+                # cannot find any more tags to cover more of the objects 
                 return result
             for obj_with_covered_tag in objects_covered_by_tag:
                 non_covered_objects.remove(obj_with_covered_tag)
@@ -135,15 +160,18 @@ def query_to_derpibooru_query(query):
     return output_string
 
 
-# Actually invoke the magic defined above on each class
+# Actually invoke the magic defined above on each image class
 for class_num in range(0, total_clusters):
+    print("------")
     input_objects_in_class= filter_elements_from_class(class_num, input_objects, class_of_each_element)
     top_common_tags = count_common_tags(input_objects_in_class).most_common(20)
     # Filter out small clusters in the image that don't have enough statistics to be useful
-    if (top_common_tags[0][1]) <= 3: # TUNABLE PARAMETER
+    if (top_common_tags[0][1]) <= cluster_size_theshold: # TUNABLE PARAMETER, see top of the file
+        print("Class " + str(class_num) + " is too small to get useful results, skipping it.")
+        print("You can tune this behavior via cluster_size_theshold variable.")
         continue
     #print(top_common_tags)
     query = assemble_query_for_objects(input_objects_in_class, all_known_tags)
     print(query_to_derpibooru_query(query))
-    print("------")
+
 
