2f9c95ca41f2c9c89fa64142a918bd693b36f110
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2016-09-16 18:36:46 +0300

    Drop unused code, better comments

--- recommend.py
+++ recommend.py
@@ -7,14 +7,6 @@ import numpy as np
 import sklearn.cluster
 
 
-# Sadly unused:
-# sklearn's agglomerative clustering requires a specific number of clusters as input,
-# and I just want to try and converge this thing iteratively and get outputs from
-# *different* stages, and ideally from a *divisive* hierarchical algorithm too.
-def distance_metric(tags1, tags2):
-    1 / (len(tags1.intersect(tags2)) ** 2)
-
-
 # Generate set of all known tags
 def list_all_tags(lists_of_tags):
     all_known_tags = set()
@@ -38,16 +30,6 @@ def tags_to_boolean_features(object_tags, all_known_tags):
 # Converts an index of an object feature into the tag string
 def feature_index_to_tag(index, all_known_tags):
     return all_known_tags[index]
-
-
-# Returns a collections.Counter object with tags
-def count_common_tags_on_feature_objects(feature_objects, all_known_tags):
-    counter = Counter()
-    for object in feature_objects:
-        for i in range(0, len(object)):
-            # exploits the fact that we have boolean features
-            counter[feature_index_to_tag(i,all_known_tags)] += object[i]
-    return counter
 
 
 # Returns a collections.Counter object with feature indices
@@ -83,8 +65,11 @@ for obj in input_objects:
 clusterer = sklearn.cluster.AffinityPropagation()
 class_of_each_element = clusterer.fit_predict(np.array(feature_objects))
 total_clusters = max(class_of_each_element)
+print("Total clusters: " + str(total_clusters))
 print(class_of_each_element)
 
+
+# Returns all objects that belong to the given class number
 def filter_elements_from_class(class_num, feature_objects, class_of_each_element):
     result = []
     assert(len(feature_objects) == len(class_of_each_element))
@@ -94,6 +79,12 @@ def filter_elements_from_class(class_num, feature_objects, class_of_each_element
     return result
 
 
+# Assembles the internal representation of sql-like query for each cluser of images
+# The internal representation is a list of tag lists; for example:
+# [ [A], [B,C] ]
+# means the query is for images that have tag A and at least one of tags B and C
+# It has to be converted to the syntax of a specific imageboard to be useful,
+# see 'query_to_derpibooru_query' function below
 def assemble_query_for_objects(input_objects, all_known_tags):
     result = []
     total_objects = len(input_objects)
@@ -104,6 +95,7 @@ def assemble_query_for_objects(input_objects, all_known_tags):
         unprocessed_tags = ordered_nonzero_tags[offset:]
         iter_result_tags = []
         non_covered_objects = deepcopy(input_objects)
+        # This loop is responsible for generating lists lists of tags with OR between them
         while len(non_covered_objects) > total_objects/6: # TUNABLE PARAMETER
             #print(len(non_covered_objects))
             # Find most common tags in objects that are not yet covered
@@ -120,8 +112,8 @@ def assemble_query_for_objects(input_objects, all_known_tags):
             iter_result_tags.append(most_common_unprocessed_tag)
             #print(iter_result_tags)
             objects_covered_by_tag = [obj for obj in non_covered_objects if (most_common_unprocessed_tag in obj)]
-            # cannot find any more tags to cover more of the objects 
             if len(objects_covered_by_tag) == 0:
+            	# cannot find any more tags to cover more of the objects 
                 return result
             for obj_with_covered_tag in objects_covered_by_tag:
                 non_covered_objects.remove(obj_with_covered_tag)
@@ -142,15 +134,15 @@ def query_to_derpibooru_query(query):
         output_string += "), "
     return output_string
 
+
+# Actually invoke the magic defined above on each class
 for class_num in range(0, total_clusters):
-    #feature_objects_in_class= filter_elements_from_class(class_num, feature_objects, class_of_each_element)
     input_objects_in_class= filter_elements_from_class(class_num, input_objects, class_of_each_element)
-    #top_common_tags = count_common_tags_on_feature_objects(feature_objects_in_class, all_known_tags)
     top_common_tags = count_common_tags(input_objects_in_class).most_common(20)
-    if (top_common_tags[0][1]) <= 3:
+    # Filter out small clusters in the image that don't have enough statistics to be useful
+    if (top_common_tags[0][1]) <= 3: # TUNABLE PARAMETER
         continue
     #print(top_common_tags)
-    #print(input_objects_in_class)
     query = assemble_query_for_objects(input_objects_in_class, all_known_tags)
     print(query_to_derpibooru_query(query))
     print("------")
